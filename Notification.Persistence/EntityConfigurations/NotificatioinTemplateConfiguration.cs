cf637e3407040d88e40cbc2846538717e0318f05
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-16 17:15:47 +0500

    doing n73's entityconfigurations

--- Notification.Persistence/EntityConfigurations/NotificatioinTemplateConfiguration.cs
+++ Notification.Persistence/EntityConfigurations/NotificatioinTemplateConfiguration.cs
@@ -1,5 +1,21 @@
-﻿namespace Notification.Persistence.EntityConfigurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notification.Domain.Entities;
+using Notification.Domain.Enums;
 
-public class NotificatioinTemplateConfiguration
+namespace Notification.Persistence.EntityConfigurations;
+
+public class NotificatioinTemplateConfiguration : IEntityTypeConfiguration<NotificationTemplate>
 {
+    public void Configure(EntityTypeBuilder<NotificationTemplate> builder)
+    {
+        builder.Property(template => template.Content)
+            .HasMaxLength(100_000);
+
+        builder
+            .ToTable("NotificationTemplates")
+            .HasDiscriminator(emailTemplate => emailTemplate.Type)
+            .HasValue<SmsTemplate>(NotificationType.Sms)
+            .HasValue<EmailTemplate>(NotificationType.Emial);
+    }
 }
--- Notification.Persistence/EntityConfigurations/NotificationHistoryConfiguration.cs
+++ Notification.Persistence/EntityConfigurations/NotificationHistoryConfiguration.cs
@@ -1,5 +1,25 @@
-﻿namespace Notification.Persistence.EntityConfigurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notification.Domain.Entities;
+using Notification.Domain.Enums;
 
-public class NotificationHistoryConfiguration
+namespace Notification.Persistence.EntityConfigurations;
+
+public class NotificationHistoryConfiguration : IEntityTypeConfiguration<NotificationHistory>
 {
+    public void Configure(EntityTypeBuilder<NotificationHistory> builder)
+    {
+        builder.Property(template => template.Content).HasMaxLength(100_000);
+
+        builder
+            .ToTable("NotificationHistories")
+            .HasDiscriminator(emailTemplate => emailTemplate.Type)
+            .HasValue<SmsHistory>(NotificationType.Sms)
+            .HasValue<EmailHistory>(NotificationType.Emial);
+
+        builder
+            .HasOne<NotificationTemplate>()
+            .WithMany(template => template.Histories)
+            .HasForeignKey(history => history.TemplateId);
+    }
 }
